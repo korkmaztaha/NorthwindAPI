cf93ee9217585fcf9da1b7b1749c7fb6b45b7f2d
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2026-04-13 09:45:39 +0300

    gateway added

---
+++ Gateway/NorthwindApi.Gateway/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!))
+        };
+    });
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
--- Presentation/NorthwindAPI.Api/Controllers/CustomersController.cs
+++ Presentation/NorthwindAPI.Api/Controllers/CustomersController.cs
@@ -10,7 +10,7 @@ using NorthwindApi.Domain.Constants;
 
 namespace NorthwindAPI.Api.Controllers;
 
-//[Authorize] 
+[Authorize] 
 [Route("api/[controller]")]
 [ApiController]
 public class CustomersController : ControllerBase
