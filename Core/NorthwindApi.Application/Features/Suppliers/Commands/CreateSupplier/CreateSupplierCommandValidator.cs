24d63639ffd79e204c8b3044ce3f3dfe55f17fb9
Taha Korkmaz <korkmaztaha@hotmail.com>
2026-04-28 10:17:52 +0300

    Suppliers validator added

---
+++ Core/NorthwindApi.Application/Features/Suppliers/Commands/CreateSupplier/CreateSupplierCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindApi.Application.Features.Suppliers.Commands.CreateSupplier
+{
+    public class CreateSupplierCommandValidator : AbstractValidator<CreateSupplierCommand>
+    {
+        public CreateSupplierCommandValidator()
+        {
+            RuleFor(x => x.CompanyName)
+                .NotEmpty().WithMessage("Company name is required.")
+                .MaximumLength(40).WithMessage("Company name must not exceed 40 characters.");
+
+            RuleFor(x => x.ContactName)
+                .MaximumLength(30).WithMessage("Contact name must not exceed 30 characters.")
+                .When(x => x.ContactName != null);
+
+            RuleFor(x => x.Phone)
+                .MaximumLength(24).WithMessage("Phone must not exceed 24 characters.")
+                .When(x => x.Phone != null);
+
+            RuleFor(x => x.Country)
+                .MaximumLength(15).WithMessage("Country must not exceed 15 characters.")
+                .When(x => x.Country != null);
+        }
+    }
+}
---
+++ Core/NorthwindApi.Application/Features/Suppliers/Commands/DeleteSupplier/DeleteSupplierCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindApi.Application.Features.Suppliers.Commands.DeleteSupplier
+{
+    public class DeleteSupplierCommandValidator : AbstractValidator<DeleteSupplierCommand>
+    {
+        public DeleteSupplierCommandValidator()
+        {
+            RuleFor(x => x.SupplierId)
+                .GreaterThan(0).WithMessage("Supplier ID must be greater than 0.");
+        }
+    }
+}
