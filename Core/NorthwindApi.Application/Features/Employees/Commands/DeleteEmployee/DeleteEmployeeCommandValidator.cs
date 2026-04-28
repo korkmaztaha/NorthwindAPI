13afa504a9518ac728b4e95ed13368d5a4388884
Taha Korkmaz <korkmaztaha@hotmail.com>
2026-04-28 12:43:01 +0300

    Employee validator added

---
+++ Core/NorthwindApi.Application/Features/Employees/Commands/DeleteEmployee/DeleteEmployeeCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindApi.Application.Features.Employees.Commands.DeleteEmployee
+{
+    public class DeleteEmployeeCommandValidator : AbstractValidator<DeleteEmployeeCommand>
+    {
+        public DeleteEmployeeCommandValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .GreaterThan(0).WithMessage("Employee ID must be greater than 0.");
+        }
+    }
+}
---
+++ Core/NorthwindApi.Application/Features/Employees/Commands/UpdateEmployee/UpdateEmployeeCommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindApi.Application.Features.Employees.Commands.UpdateEmployee
+{
+    public class UpdateEmployeeCommandValidator : AbstractValidator<UpdateEmployeeCommand>
+    {
+        public UpdateEmployeeCommandValidator()
+        {
+            RuleFor(x => x.EmployeeId)
+                .GreaterThan(0).WithMessage("Employee ID must be greater than 0.");
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(10).WithMessage("First name must not exceed 10 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(20).WithMessage("Last name must not exceed 20 characters.");
+
+            RuleFor(x => x.Title)
+                .MaximumLength(30).WithMessage("Title must not exceed 30 characters.")
+                .When(x => x.Title != null);
+
+            RuleFor(x => x.BirthDate)
+                .LessThan(DateTime.UtcNow).WithMessage("Birth date cannot be in the future.")
+                .When(x => x.BirthDate.HasValue);
+
+            RuleFor(x => x.HireDate)
+                .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Hire date cannot be in the future.")
+                .When(x => x.HireDate.HasValue);
+
+            RuleFor(x => x.ReportsTo)
+                .GreaterThan(0).WithMessage("Reports to must be greater than 0.")
+                .When(x => x.ReportsTo.HasValue);
+        }
+    }
+}
