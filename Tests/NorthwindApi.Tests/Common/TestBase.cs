0da69c23994c05e33ffa07f3f893735127ea9a94
Taha Korkmaz <korkmaztaha@hotmail.com>
2026-04-30 10:35:15 +0300

    testbase added

---
+++ Tests/NorthwindApi.Tests/Common/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using NorthwindApi.Application.Interfaces.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindApi.Tests.Common
+{
+    public abstract class TestBase
+    {
+        protected readonly Mock<IUnitOfWork> MockUnitOfWork;
+
+        protected TestBase()
+        {
+            MockUnitOfWork = new Mock<IUnitOfWork>();
+        }
+    }
+}
